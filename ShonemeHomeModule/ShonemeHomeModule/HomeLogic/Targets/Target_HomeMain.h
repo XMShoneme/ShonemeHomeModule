7db583b7cefd3fe38d96c508fc22afcfa740f08d
iOSmiling <kunlongxue@gmail.com>
2017-05-10 16:11:39 +0800

    fix

--- ShonemeHomeModule/ShonemeHomeModule/HomeLogic/Targets/Target_HomeMain.h
+++ ShonemeHomeModule/ShonemeHomeModule/HomeLogic/Targets/Target_HomeMain.h
@@ -2,6 +2,13 @@
 //  Target_HomeMain.h
 //  ShonemeHomeModule
 //
+/*
+    params
+    key => key //key的取名 是 key
+    value = >anything
+ */
+
+
 //  Created by 薛坤龙 on 2017/5/10.
 //  Copyright © 2017年 xm. All rights reserved.
 //
@@ -11,6 +18,7 @@
 
 @interface Target_HomeMain : NSObject
 
+//去model化
 - (UIViewController *)Action_viewControllerWithDic:(NSDictionary *)params;
 
 @end
